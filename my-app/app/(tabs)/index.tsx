8defac8ea762c58c015e90c8d48433ed75806ca8
Chandra <adityanugraha7251@gmail.com>
2025-12-16 12:33:34 +0800

    awal nyoba

--- my-app/app/(tabs)/index.tsx
+++ my-app/app/(tabs)/index.tsx
@@ -1,10 +1,57 @@
-import { View , Text , StyleSheet} from 'react-native'
+import { View , Text , StyleSheet, ImageBackground} from 'react-native'
+
+
 const app = () => {
   return (
-    <View>
-      <Text>One Rental Bali</Text>
+    <View style={styles.container}>
+      <ImageBackground 
+        source={require('@/assets/images/unudBg.jpg')}
+        resizeMode="cover"
+        style = {styles.image}
+      >
+        <div style={styles.card}>
+
+        </div>
+        <Text style={styles.text}>
+          myUdayana
+        </Text>
+      </ImageBackground>
+
+      <div>
+
+      </div>
+      
     </View>
   )
 }
 
 export default app
+
+const styles = StyleSheet.create({
+  container : {
+    flex : 1,
+    flexDirection: 'column'
+  },
+  text : {
+    color: 'white',
+    textAlign: 'center',
+    fontSize: 50,
+    fontWeight: 'bold'
+  },
+  image:{
+    width: "100%",
+    height: "50%",
+    flex: 1,
+    resizeMode: "contain",
+    justifyContent:"center"
+  },
+  card : {
+    height:"30%",
+    width:"90%",
+    backgroundColor:"white",
+    borderCurve: "circular",
+    position: "relative"
+    
+
+  }
+})
